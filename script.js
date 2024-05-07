cbc3c55303a73c9ad04cbdb52ac3ff9f74082bd8
emilirol <150665592+emilirol@users.noreply.github.com>
2024-05-07 10:08:43 +0200

    Making a onOff button and editing JS

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
+/*function toggleButton(){
+    let onOff = document.querySelector(".onOff__button--button");
+    if (onOff.classList.contains(".off")){
+        onOff.classList.remove(".off");
+        onOff.classList.add(".on");
+        onOff.textContent = "Animations ON";
+
+    } else{
+        onOff.classList.remove(".on");
+        onOff.classList.add(".off");
+        onOff.textContent = "Animations OFF";
+    }
+
+}
+
+
 const watching = document.querySelector();
 
 const options ={
     root: document.querySelector()
     rootMargin:""
     threshold:[]
-}
+}*/
